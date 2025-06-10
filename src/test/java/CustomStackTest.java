e6eda64dd29166b4b953c6920343c79b737a2f62
santos rafaelo <santoschrist1234@gmail.com>
2025-06-10 10:08:48 +0200

    test: add failing test for push(T)

--- src/test/java/CustomStackTest.java
+++ src/test/java/CustomStackTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import smartappdev.module_2_tdd.CustomStack;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CustomStackTest{
@@ -15,5 +16,11 @@ public class CustomStackTest{
         assertTrue(result, "Stack should be empty");
     }
 
+    @Test
+    public void pushStackTest(){
+        customStack.push("Apples and Oranges");
+        boolean result = customStack.isEmpty();
+         assertFalse(result, "Stack should not be empty");
+    }
 
 }
