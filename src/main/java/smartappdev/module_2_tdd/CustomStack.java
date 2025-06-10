a0f443e4e65fe124493f66a33d824dc701b704b2
santos rafaelo <santoschrist1234@gmail.com>
2025-06-10 09:44:22 +0200

    test: add failing test for isEmpty()

---
+++ src/main/java/smartappdev/module_2_tdd/CustomStack.java
@@ -0,0 +1,4 @@
+package smartappdev.module_2_tdd;
+
+public class CustomStack {
+}
---
+++ src/test/java/CustomStackTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+import smartappdev.module_2_tdd.CustomStack;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CustomStackTest {
+
+    CustomStack customStack = new CustomStack();
+
+    @Test
+    public void emptyStackTest() {
+        boolean result = customStack.isEmpty();
+        assertTrue(result, "Stack should be empty");
+    }
+}
