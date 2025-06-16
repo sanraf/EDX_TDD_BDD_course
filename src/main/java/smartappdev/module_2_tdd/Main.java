772c4b79fd0c7da11af16b4f116d052e9359b421
santos rafaelo <santoschrist1234@gmail.com>
2025-06-16 08:49:50 +0200

    modify:created main class for module 2

--- src/main/java/smartappdev/module_2_tdd/Main.java
+++ src/main/java/smartappdev/module_2_tdd/Main.java
@@ -1,4 +1,4 @@
-package smartappdev;
+package smartappdev.module_2_tdd;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
